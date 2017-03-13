62abfdaf22285279a5b6287cae14898713c827d6
Jedd Cuison <jedd.cuison@cerner.com>
2017-03-13 15:35:54 -0400

    Rename package testSuiteClass to testsuite

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/PreFlightTest.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/PreFlightTest.java
@@ -1,4 +1,4 @@
-package com.cerner.jwala.ui.selenium.testSuiteClasses;
+package com.cerner.jwala.ui.selenium.testsuite;
 
 import org.junit.Test;
 
--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/configuration/resources/UploadResourceTest.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/configuration/resources/UploadResourceTest.java
@@ -1,6 +1,6 @@
-package com.cerner.jwala.ui.selenium.testSuiteClasses.configuration.resources;
+package com.cerner.jwala.ui.selenium.testsuite.configuration.resources;
 
-import com.cerner.jwala.ui.selenium.testSuiteClasses.JwalaTest;
+import com.cerner.jwala.ui.selenium.testsuite.JwalaTest;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
