4b57853e6d45c2c811717a6e693d33d295b5d943
Jedd Cuison <jedd.cuison@cerner.com>
2017-03-24 15:48:44 -0400

    Fix bug wherein the lowest portion of the editor is hidden

--- jwala-webapp/src/main/webapp/resources/js/react/CodeMirrorComponent.js
+++ jwala-webapp/src/main/webapp/resources/js/react/CodeMirrorComponent.js
@@ -45,8 +45,7 @@ var CodeMirrorComponent = React.createClass({
     resize: function() {
         var textAreaHeight = $(this.refs.theContainer.getDOMNode()).height() - $(this.refs.theToolbar.getDOMNode()).height() -
                     CodeMirrorComponent.SPLITTER_DISTANCE_FROM_TOOLBAR;
-        console.log(textAreaHeight);
-        $(".CodeMirror.cm-s-default").css("height", textAreaHeight);
+        $(".CodeMirror.cm-s-default").css("height", $(".horz-divider.rsplitter.childContainer.vert").height() - $(".RToolBar").height() - 35);
     },
     onChanged: function() {
         this.refs.theToolbar.refs.saveBtn.setEnabled(this.isContentChanged());
@@ -57,8 +56,5 @@ var CodeMirrorComponent = React.createClass({
         this.codeMirror.setValue(data);
         this.codeMirror.on("change", this.onChanged);
         this.state.data = this.codeMirror.getValue();
-    },
-    statics: {
-        SPLITTER_DISTANCE_FROM_TOOLBAR: 19
     }
 });
