0d4d31c51302a68c8f4e2f8d2314a759e5992f8a
Barve <SB053052@cerner.net>
2017-04-04 10:39:38 -0400

    Changed comment format

--- jwala-common/src/main/java/com/cerner/jwala/common/rule/group/GroupNameRule.java
+++ jwala-common/src/main/java/com/cerner/jwala/common/rule/group/GroupNameRule.java
@@ -21,12 +21,13 @@ public class GroupNameRule extends ValidNameRule implements Rule {
         return "Invalid Group Name: \"" + name + "\"";
     }
 
-    /*
-    1)Checks for null
-    2)Checks for an empty string +only spaces
-    3)checks for a string with last character space
-    4)checks for a string with first character space
-    5) checks that the string only contains alphanumeric+period+dash+underscore+space
+    /**
+     * 1)Checks for null
+     2)Checks for an empty string +only spaces
+     3)checks for a string with last character space
+     4)checks for a string with first character space
+     5) checks that the string only contains alphanumeric+period+dash+underscore+space
+     * @return boolean
      */
     @Override
     public boolean isValid() {return name != null && !"".equals(name.trim()) && !name.matches(".*[\\s]$") && !name.matches("^[\\s].*") && name.matches("[A-Za-z0-9._\\s-]+");
