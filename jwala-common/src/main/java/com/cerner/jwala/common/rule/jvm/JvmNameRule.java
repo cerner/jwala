f03f376abae3f3869ac1795193b4df27f4ee56b2
Barve <SB053052@cerner.net>
2017-04-04 10:39:38 -0400

    Changed comment format

--- jwala-common/src/main/java/com/cerner/jwala/common/rule/jvm/JvmNameRule.java
+++ jwala-common/src/main/java/com/cerner/jwala/common/rule/jvm/JvmNameRule.java
@@ -20,12 +20,13 @@ public class JvmNameRule extends ValidNameRule {
         return "Invalid Jvm Name : \"" + name + "\"";
     }
 
-    /*
-    1)Checks for null
-    2)Checks for an empty string +only spaces
-    3)checks for a string with last character space
-    4)checks for a string with first character space
-    5) checks that the string only contains alphanumeric+period+dash+underscore+space
+    /**
+     * 1)Checks for null
+     2)Checks for an empty string +only spaces
+     3)checks for a string with last character space
+     4)checks for a string with first character space
+     5) checks that the string only contains alphanumeric+period+dash+underscore+space
+     * @return boolean
      */
     @Override
     public boolean isValid() {
