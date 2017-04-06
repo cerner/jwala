66ab3db2bac7673d8637bf97889cc65e5032bb30
Jedd Cuison <jedd.cuison@cerner.com>
2017-04-06 08:59:44 -0400

    Do not write logs in the history table and only log a response of not ok once

--- jwala-services/src/main/java/com/cerner/jwala/service/jvm/impl/spring/component/JvmStateResolverWorker.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/jvm/impl/spring/component/JvmStateResolverWorker.java
@@ -58,18 +58,16 @@ public class JvmStateResolverWorker {
             response = clientFactoryHelper.requestGet(jvm.getStatusUri());
             LOGGER.debug(">>> Response = {} from JVM {}", response.getStatusCode(), jvm.getId().getId());
 
+            if (!JvmState.JVM_STARTED.equals(jvm.getState()) && !HttpStatus.OK.equals(response.getStatusCode())) {
+                LOGGER.warn("Request {} sent expecting a response code of {} but got {} instead",
+                        jvm.getStatusUri(), HttpStatus.OK.value(), response.getRawStatusCode());
+            }
+
             jvmStateService.updateNotInMemOrStaleState(jvm, JvmState.JVM_STARTED, StringUtils.EMPTY);
             currentState = new CurrentState<>(jvm.getId(), JvmState.JVM_STARTED, DateTime.now(), StateType.JVM);
 
-            if (response.getStatusCode() != HttpStatus.OK) {
-                final String msg = MessageFormat.format("Request {0} sent expecting a response code of {1} but got {2} instead",
-                        jvm.getStatusUri(), HttpStatus.OK.value(), response.getRawStatusCode());
-                historyFacadeService.write(jvm.getJvmName(), jvm.getGroups(), msg, EventType.SYSTEM_ERROR, "");
-            }
-
         } catch (final IOException ioe) {
             LOGGER.warn("{} {} {}", jvm.getJvmName(), ioe.getMessage(), "Setting JVM state to STOPPED.", ioe);
-            historyFacadeService.write(jvm.getJvmName(), jvm.getGroups(), ioe.getMessage(), EventType.SYSTEM_INFO, "");
             jvmStateService.updateNotInMemOrStaleState(jvm, JvmState.JVM_STOPPED, StringUtils.EMPTY);
             currentState = new CurrentState<>(jvm.getId(), JvmState.JVM_STOPPED, DateTime.now(), StateType.JVM);
         } catch (final RuntimeException rte) {
