404e93aba940eb7d254564ba40094a14748c5b44
Barve <SB053052@cerner.net>
2017-04-17 13:55:13 -0400

    Added history logging statement

--- jwala-services/src/main/java/com/cerner/jwala/service/app/impl/ApplicationServiceImpl.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/app/impl/ApplicationServiceImpl.java
@@ -220,6 +220,7 @@ public class ApplicationServiceImpl implements ApplicationService {
                         "The target JVM must be stopped before attempting to update the resource files");
             }
             final String hostName = jvm.getHostName();
+            historyFacadeService.write("", jvm.getGroups(), "Generating and Deploying \"" + appName + "\" resources", EventType.USER_ACTION_INFO, user.getId());
             return resourceService.generateAndDeployFile(resourceIdentifier, appName + "-" + jvmName, resourceTemplateName, hostName);
         } catch (ResourceFileGeneratorException e) {
             LOGGER.error("Fail to generate the resource file {}", resourceTemplateName, e);
