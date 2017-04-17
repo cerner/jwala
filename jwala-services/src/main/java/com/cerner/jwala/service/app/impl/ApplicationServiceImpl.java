d6335402f26d901b622049ecbbc32913aad63014
Barve <SB053052@cerner.net>
2017-04-17 17:28:28 -0400

    Getting a single group and changed logging message

--- jwala-services/src/main/java/com/cerner/jwala/service/app/impl/ApplicationServiceImpl.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/app/impl/ApplicationServiceImpl.java
@@ -220,7 +220,8 @@ public class ApplicationServiceImpl implements ApplicationService {
                         "The target JVM must be stopped before attempting to update the resource files");
             }
             final String hostName = jvm.getHostName();
-            historyFacadeService.write("", jvm.getGroups(), "Generating and Deploying \"" + appName + "\" resources", EventType.USER_ACTION_INFO, user.getId());
+            Group group=groupPersistenceService.getGroup(groupName);
+            historyFacadeService.write("", group, "Generating and Deploying resource files in webapp\"" + appName + "\" resources", EventType.USER_ACTION_INFO, user.getId());
             return resourceService.generateAndDeployFile(resourceIdentifier, appName + "-" + jvmName, resourceTemplateName, hostName);
         } catch (ResourceFileGeneratorException e) {
             LOGGER.error("Fail to generate the resource file {}", resourceTemplateName, e);
