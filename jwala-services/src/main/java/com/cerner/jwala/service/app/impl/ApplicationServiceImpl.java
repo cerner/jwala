94f6a37700ed2a4dc25d51169defc4756295f2ab
akinny <akinny@users.noreply.github.com>
2017-04-19 13:41:06 -0400

    Add a null check

--- jwala-services/src/main/java/com/cerner/jwala/service/app/impl/ApplicationServiceImpl.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/app/impl/ApplicationServiceImpl.java
@@ -139,7 +139,7 @@ public class ApplicationServiceImpl implements ApplicationService {
 
     private void updateApplicationWarMetaData(UpdateApplicationRequest updateApplicationRequest, Application application) {
         final String appWarName = application.getWarName();
-        if (!appWarName.isEmpty()) {
+        if (appWarName!=null && !appWarName.isEmpty()) {
             final String appName = application.getName();
             try {
                 String originalJsonMetaData = groupPersistenceService.getGroupAppResourceTemplateMetaData(application.getGroup().getName(), appWarName);
