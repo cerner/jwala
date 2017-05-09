53422874a65ccfda3b34ec8362f8cd22ee295557
Sharvari Barve <Sharvari.barve@cerner.com>
2017-05-09 11:26:14 -0400

    Added validation for length of JVM name (#91)
    * Added validation for length of JVM name
    
    * Created constant called MAX_LENGTH
    
    * Added test for 64 character Name
    
    * Changed error message
    
    * Added proper spacing
    
    * Added property Key for max_length of JVm
    
    * Changed propertyName and added message to notify user about this

--- jwala-common/src/main/java/com/cerner/jwala/common/rule/jvm/JvmNameRule.java
+++ jwala-common/src/main/java/com/cerner/jwala/common/rule/jvm/JvmNameRule.java
@@ -1,10 +1,15 @@
 package com.cerner.jwala.common.rule.jvm;
 
+import com.cerner.jwala.common.domain.model.app.Application;
 import com.cerner.jwala.common.domain.model.fault.FaultType;
 import com.cerner.jwala.common.exception.MessageResponseStatus;
+import com.cerner.jwala.common.properties.ApplicationProperties;
+import com.cerner.jwala.common.properties.PropertyKeys;
 import com.cerner.jwala.common.rule.ValidNameRule;
 
 public class JvmNameRule extends ValidNameRule {
+
+    public final int MAX_LENGTH = ApplicationProperties.getAsInteger(PropertyKeys.JVM_ROUTE_MAX_LENGTH, 64);
 
     public JvmNameRule(final String theName) {
         super(theName);
@@ -16,8 +21,16 @@ public class JvmNameRule extends ValidNameRule {
     }
 
     @Override
+    public boolean isValid() {
+        return super.isValid() && name.length() <= MAX_LENGTH;
+    }
+
+    @Override
     protected String getMessage() {
-        return "Invalid Jvm Name : \"" + name + "\"";
+        return name != null && name.length() > MAX_LENGTH ? "Length of JVM should not exceed " + MAX_LENGTH + " " +
+                "characters, however JVM_ROUTE_MAX_LENGTH property can be overriden to allow for longer length." :
+                "Invalid Jvm Name : \"" +
+                name + "\"";
     }
 
 }
