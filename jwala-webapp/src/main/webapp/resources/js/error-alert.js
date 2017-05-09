f4b5060e4253fd7e083187e9bf5e16ec502ee640
Barve <SB053052@cerner.net>
2017-05-09 10:49:20 -0400

    Iterating over content object to display individual properties

--- jwala-webapp/src/main/webapp/resources/js/error-alert.js
+++ jwala-webapp/src/main/webapp/resources/js/error-alert.js
@@ -46,6 +46,14 @@ $.extend({ errorAlert: function (message, dlgTitle, modal, content) {
 
             var detailsHtml = "";
             if (content && content !== "") {
+            let details = content;
+                 if(!Array.isArray(content)){
+                 details = new Array(content.length);
+                 for(let key in content){
+                 details.push(content[key]);
+                 }
+                 }
+                content = details;
                 var showErrorDetails = "$('.showErrorDetails').hide();$('.hideErrorDetails').show();$('.stackTrace').show()";
                 var hideErrorDetails = "$('.showErrorDetails').show();$('.hideErrorDetails').hide();$('.stackTrace').hide()";
                 detailsHtml = "<br><br><div style='width:100%;text-align:left'><button class='showErrorDetails' onClick="
