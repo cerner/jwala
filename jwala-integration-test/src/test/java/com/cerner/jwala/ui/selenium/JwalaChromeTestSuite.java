1e00ed7cae9368c1cedd53b887df46b77c553fd8
Rahul Sayini <rahul.sayini@cerner.com>
2017-05-22 23:56:43 -0400

    fix webapp search and add selenium test case (#111)
    * fix webapp search and add selenium test case
    
    * Refactor Selenium test case
    
    Details
    
    - remove Thread.sleep where not needed
    - put test data creation in init method
    - put test data removal in destroy method
    - use parent methods to simplify element events such as click, clickWhenReady, sendKeys etc...
    - refresh the browser to display all rows again after filtering
    - use click instead of waitClick when waitClick is not required

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/JwalaChromeTestSuite.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/JwalaChromeTestSuite.java
@@ -4,6 +4,7 @@ import com.cerner.jwala.ui.selenium.testsuite.LoginTest;
 import com.cerner.jwala.ui.selenium.testsuite.LogoutTest;
 import com.cerner.jwala.ui.selenium.testsuite.PreFlightTest;
 import com.cerner.jwala.ui.selenium.testsuite.configuration.app.AppCreateTest;
+import com.cerner.jwala.ui.selenium.testsuite.configuration.app.AppSearchTest;
 import com.cerner.jwala.ui.selenium.testsuite.configuration.app.AppDeleteTest;
 import com.cerner.jwala.ui.selenium.testsuite.configuration.group.GroupCreateTest;
 import com.cerner.jwala.ui.selenium.testsuite.configuration.group.GroupDeleteTest;
@@ -30,7 +31,7 @@ import java.util.concurrent.TimeUnit;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({PreFlightTest.class, LoginTest.class, GroupCreateTest.class, JvmCreateTest.class, WebServerCreateTest.class,
-        AppCreateTest.class, ResourceTopologyTest.class, UploadResourceTest.class, AddExternalProperty.class,
+        AppCreateTest.class, AppSearchTest.class, ResourceTopologyTest.class, UploadResourceTest.class, AddExternalProperty.class,
         ModifyExternalPropertyResource.class, DeleteExternalProperty.class, ResourceDeployTest.class, HistoryTablePopupTest.class,
         AppDeleteTest.class, JvmOperationsPageDeleteTest.class, WebServerOperationsPageDelete.class, WebServerCreateTest.class,
         WebServerDeleteTest.class, JvmCreateTest.class, JvmDeleteTest.class, GroupDeleteTest.class, BalancerManagerTest.class,
