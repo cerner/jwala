f2ab9293c5471a749ca4254aaaf7aab2904b8d84
Jedd Cuison <jedd.cuison@cerner.com>
2017-05-22 23:50:53 -0400

    Refactor Selenium test case
    Details
    
    - remove Thread.sleep where not needed
    - put test data creation in init method
    - put test data removal in destroy method
    - use parent methods to simplify element events such as click, clickWhenReady, sendKeys etc...
    - refresh the browser to display all rows again after filtering
    - use click instead of waitClick when waitClick is not required

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/configuration/app/AppSearchTest.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/configuration/app/AppSearchTest.java
@@ -1,71 +1,84 @@
 package com.cerner.jwala.ui.selenium.testsuite.configuration.app;
 
 import com.cerner.jwala.ui.selenium.testsuite.JwalaTest;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by RS045609 on 5/5/2017.
+ * Created by Rahul Sayini on 5/5/2017
  */
 public class AppSearchTest extends JwalaTest {
     private static final String GROUP_NAME_1 = "zGroup1-" + CURRENT_TIME_MILLIS;
-    private static final String APP_NAME1 = "zAppSearch1-" + CURRENT_TIME_MILLIS;
-    private static final String APP_NAME2 = "zAppSearch2-" + CURRENT_TIME_MILLIS;
 
-    private static final String searchApp1 = "Search1-" + CURRENT_TIME_MILLIS;
-    private static final String searchApp2 = "Search2-" + CURRENT_TIME_MILLIS;
+    private static final String APP_NAME_1 = "zAppSearch1-" + CURRENT_TIME_MILLIS;
+    private static final String APP_NAME_2 = "zAppSearch2-" + CURRENT_TIME_MILLIS;
 
+    private static final String SEARCH_APP_1 = "Search1-" + CURRENT_TIME_MILLIS;
+    private static final String SEARCH_APP_2 = "Search2-" + CURRENT_TIME_MILLIS;
 
-    @Test
-    public void testSearchApp() throws InterruptedException {
+    @Before
+    public void init() throws InterruptedException {
         clickTab("Configuration");
         clickTab("Web Apps");
-        Thread.sleep(300);
-        driver.findElement(By.xpath("//button[span[text()='Add']]")).click();
-        Thread.sleep(1000); // give a little time for the first element to get into focus as required by how
-        // "focus on the first element" was implemented here
-        driver.switchTo().activeElement().sendKeys(APP_NAME1);
-        driver.switchTo().activeElement().sendKeys(Keys.TAB);
-        driver.switchTo().activeElement().sendKeys("/zApp1");
-        driver.findElement(By.xpath("//div[contains(text(), '" + GROUP_NAME_1 + "')]/input")).click();
-        driver.findElement(By.xpath("//button[span[text()='Ok']]")).click();
-        Thread.sleep(300);
-        driver.findElement(By.xpath("//button[span[text()='Add']]")).click();
+
+        click(By.xpath("//button[span[text()='Add']]"));
         Thread.sleep(300); // give a little time for the first element to get into focus as required by how
-        // "focus on the first element" was implemented here
-        driver.switchTo().activeElement().sendKeys(APP_NAME2);
-        driver.switchTo().activeElement().sendKeys(Keys.TAB);
-        driver.switchTo().activeElement().sendKeys("/zApp2");
-        driver.findElement(By.xpath("//div[contains(text(), '" + GROUP_NAME_1 + "')]/input")).click();
-        driver.findElement(By.xpath("//button[span[text()='Ok']]")).click();
+        sendKeys(APP_NAME_1);
+        sendKeys(Keys.TAB);
+        sendKeys("/zApp1");
+        click(By.xpath("//div[contains(text(), '" + GROUP_NAME_1 + "')]/input"));
+        click(By.xpath("//button[span[text()='Ok']]"));
+        waitUntilElementIsVisible(By.xpath("//button[text()='" + APP_NAME_1 + "']"));
 
-        driver.findElement(By.xpath("//span[contains(text(), 'Search')]/following-sibling::input")).click();
-        driver.switchTo().activeElement().sendKeys(searchApp1);
-        driver.findElement(By.xpath("//button[text()='" + APP_NAME1 + "']"));
-        isElementExists("//div[contains(text(),'Showing 1 to 1 of 1')]");
-
-        driver.findElement(By.xpath("//span[contains(text(), 'Search')]/following-sibling::input")).clear();
-        driver.switchTo().activeElement().sendKeys(searchApp2);
-        driver.findElement(By.xpath("//button[text()='" + APP_NAME2 + "']"));
-        driver.findElement(By.xpath("//span[contains(text(), 'Search')]/following-sibling::input")).clear();
-        isElementExists("//div[contains(text(),'Showing 1 to 1 of 1')]");
-
-        driver.switchTo().activeElement().sendKeys("zApp");
-
-        waitClick(By.xpath("//tr[td[button[text()='" + APP_NAME1 + "']]]"));
-        waitClick(By.xpath("//button[span[text()='Delete']]"));
-        waitClick(By.xpath("//button[span[text()='Yes']]"));
-        new WebDriverWait(driver, 5)
-                .until(ExpectedConditions.numberOfElementsToBe(By.xpath("//button[text()='" + APP_NAME1 + "']"), 0));
-
-
-        waitClick(By.xpath("//tr[td[button[text()='" + APP_NAME2 + "']]]"));
-        waitClick(By.xpath("//button[span[text()='Delete']]"));
-        waitClick(By.xpath("//button[span[text()='Yes']]"));
-        new WebDriverWait(driver, 5)
-                .until(ExpectedConditions.numberOfElementsToBe(By.xpath("//button[text()='" + APP_NAME2 + "']"), 0));
+        click(By.xpath("//button[span[text()='Add']]"));
+        Thread.sleep(300); // give a little time for the first element to get into focus as required by how
+        sendKeys(APP_NAME_2);
+        sendKeys(Keys.TAB);
+        sendKeys("/zApp2");
+        click(By.xpath("//div[contains(text(), '" + GROUP_NAME_1 + "')]/input"));
+        click(By.xpath("//button[span[text()='Ok']]"));
+        waitUntilElementIsVisible(By.xpath("//button[text()='" + APP_NAME_2 + "']"));
     }
+
+    @After
+    public void destroy() {
+        // refresh to make sure all rows are visible again
+        // executing clear for the search box will not show
+        // the rows
+        driver.navigate().refresh(); // refresh to make sure all rows are visible again
+
+        clickWhenReady(By.xpath("//tr[td[button[text()='" + APP_NAME_1 + "']]]"));
+        click(By.xpath("//button[span[text()='Delete']]"));
+        click(By.xpath("//button[span[text()='Yes']]"));
+        waitUntilElementIsNotVisible(By.xpath("//button[text()='" + APP_NAME_1 + "']"));
+
+        click(By.xpath("//tr[td[button[text()='" + APP_NAME_2 + "']]]"));
+        click(By.xpath("//button[span[text()='Delete']]"));
+        click(By.xpath("//button[span[text()='Yes']]"));
+        waitUntilElementIsNotVisible(By.xpath("//button[text()='" + APP_NAME_2 + "']"));
+    }
+
+    @Test
+    public void testSearchApp() {
+        click(By.xpath("//span[contains(text(), 'Search')]/following-sibling::input"));
+        sendKeys(SEARCH_APP_1);
+        assertTrue(isElementExists("//button[text()='" + APP_NAME_1 + "']"));
+        assertTrue(isElementExists("//div[contains(text(),'Showing 1 to 1 of 1')]"));
+
+        // refresh to make sure all rows are visible again
+        // executing clear for the search box will not show
+        // the rows
+        driver.navigate().refresh();
+
+        click(By.xpath("//span[contains(text(), 'Search')]/following-sibling::input"));
+        sendKeys(SEARCH_APP_2);
+        assertTrue(isElementExists("//button[text()='" + APP_NAME_2 + "']"));
+        assertTrue(isElementExists("//div[contains(text(),'Showing 1 to 1 of 1')]"));
+    }
+
 }
