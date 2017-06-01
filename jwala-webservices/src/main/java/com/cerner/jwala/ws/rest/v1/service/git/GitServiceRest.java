0e3c3bb6e4f6e85fecca09434bb35ba31f3c06bd
rahul <Rahul.Sayini@cerner.com>
2017-06-01 13:53:49 -0400

    add more apis

--- jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/git/GitServiceRest.java
+++ jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/git/GitServiceRest.java
@@ -2,10 +2,7 @@ package com.cerner.jwala.ws.rest.v1.service.git;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 
-import javax.ws.rs.MatrixParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -13,11 +10,15 @@ import javax.ws.rs.core.Response;
  * Created by RS045609 on 6/1/2017.
  */
 @Path("/git")
-@Produces(MediaType.APPLICATION_JSON)
 public interface GitServiceRest {
 
     @POST
     @Path("/clone")
+    @Consumes(MediaType.TEXT_PLAIN)
     Response.Status cloneRepository(@MatrixParam("gitUri") String gitUri) throws GitAPIException;
+
+    @GET
+    @Path("/")
+    Response.Status getSomething();
 
 }
