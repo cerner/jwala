01557b4483dff8b403ea0fdab5532baef1a9d7af
rahul <Rahul.Sayini@cerner.com>
2017-06-01 18:40:09 -0400

    add GitServiceRest for git clone

--- jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/git/impl/GitServiceRestImpl.java
+++ jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/git/impl/GitServiceRestImpl.java
@@ -6,8 +6,8 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.FormParam;
 import javax.ws.rs.core.Response;
+import java.io.File;
 
 /**
  * Created by RS045609 on 6/1/2017.
@@ -17,19 +17,22 @@ public class GitServiceRestImpl implements GitServiceRest {
     private static final Logger LOGGER = LoggerFactory.getLogger(GitServiceRestImpl.class);
 
     @Override
-    public Response.Status cloneRepository(@FormParam("gitUri") String gitUri) throws GitAPIException {
-        Git git = Git.cloneRepository()
-                .setURI("https://github.com/praveensayani/Online-Career-Center.git")
-                .call();
-        LOGGER.info("sharvari : ", git);
-
+    public Response.Status cloneRepository(String gitUri, String path) {
+        LOGGER.info("The gitUri {0} and destination paths{1}", gitUri, path);
+        Git git = null;
+        if(path == null){
+            path = "";
+        }
+        File file = new File(path);
+        try {
+            git = Git.cloneRepository()
+                    .setURI(gitUri)
+                    .setDirectory(file)
+                    .call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
         return Response.Status.ACCEPTED;
     }
 
-    @Override
-    public Response.Status getSomething() {
-        return Response.Status.ACCEPTED;
-    }
-
-    
 }
