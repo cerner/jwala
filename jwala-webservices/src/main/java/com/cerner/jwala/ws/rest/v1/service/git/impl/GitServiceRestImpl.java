d1666cbbac97f9561c33cd6b174c288d62c6c886
rahul <Rahul.Sayini@cerner.com>
2017-06-01 14:43:47 -0400

    made some changes to GitServiceRestImpl.java

--- jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/git/impl/GitServiceRestImpl.java
+++ jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/git/impl/GitServiceRestImpl.java
@@ -3,19 +3,26 @@ package com.cerner.jwala.ws.rest.v1.service.git.impl;
 import com.cerner.jwala.ws.rest.v1.service.git.GitServiceRest;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.MatrixParam;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.core.Response;
 
 /**
  * Created by RS045609 on 6/1/2017.
  */
 public class GitServiceRestImpl implements GitServiceRest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GitServiceRestImpl.class);
+
     @Override
-    public Response.Status cloneRepository (@MatrixParam("gitUri") String gitUri) throws GitAPIException {
+    public Response.Status cloneRepository(@FormParam("gitUri") String gitUri) throws GitAPIException {
         Git git = Git.cloneRepository()
-                .setURI(gitUri)
+                .setURI("https://github.com/praveensayani/Online-Career-Center.git")
                 .call();
+        LOGGER.info("sharvari : ", git);
+
         return Response.Status.ACCEPTED;
     }
 
@@ -24,4 +31,5 @@ public class GitServiceRestImpl implements GitServiceRest {
         return Response.Status.ACCEPTED;
     }
 
+    
 }
