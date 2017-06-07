e42a87e5077ae30223ecb779859017097e3e7653
Sharvari Barve <Sharvari.barve@cerner.com>
2017-06-07 17:34:25 -0400

    REST Resources API to handle conditions being case insensitive (#127)
    * added Lower case conversion in Named queries
    
    * Addressed code review by Formatting
    
    * Changed formatting
    
    * Changed formatting

--- jwala-persistence/src/main/java/com/cerner/jwala/persistence/jpa/domain/resource/config/template/JpaWebServerConfigTemplate.java
+++ jwala-persistence/src/main/java/com/cerner/jwala/persistence/jpa/domain/resource/config/template/JpaWebServerConfigTemplate.java
@@ -13,28 +13,40 @@ import javax.persistence.*;
 @Table(name = "WEBSERVER_CONFIG_TEMPLATE", uniqueConstraints = {@UniqueConstraint(columnNames = {"WEBSERVER_ID", "TEMPLATE_NAME"})})
 @NamedQueries({
         @NamedQuery(name = JpaWebServerConfigTemplate.GET_WEBSERVER_RESOURCE_TEMPLATE_NAMES,
-                query = "SELECT t.templateName FROM JpaWebServerConfigTemplate t WHERE t.webServer.name = :webServerName"),
+                query = "SELECT t.templateName FROM JpaWebServerConfigTemplate t WHERE LOWER(t.webServer.name) = " +
+                        "LOWER(:webServerName)"),
         @NamedQuery(name = JpaWebServerConfigTemplate.GET_WEBSERVER_TEMPLATE_CONTENT,
-                query = "SELECT t.templateContent FROM JpaWebServerConfigTemplate t where t.webServer.name = :webServerName and t.templateName = :templateName"),
+                query = "SELECT t.templateContent FROM JpaWebServerConfigTemplate t where LOWER(t.webServer.name) = " +
+                        "LOWER(:webServerName) and t.templateName = :templateName"),
         @NamedQuery(name = JpaWebServerConfigTemplate.GET_WEBSERVER_TEMPLATE_METADATA,
-                query = "SELECT t.metaData FROM JpaWebServerConfigTemplate t where t.webServer.name = :webServerName and t.templateName = :templateName"),
+                query = "SELECT t.metaData FROM JpaWebServerConfigTemplate t where LOWER(t.webServer.name) = " +
+                        "LOWER(:webServerName) and t.templateName = :templateName"),
         @NamedQuery(name = JpaWebServerConfigTemplate.UPDATE_WEBSERVER_TEMPLATE_CONTENT,
-                query = "UPDATE JpaWebServerConfigTemplate t SET t.templateContent = :templateContent WHERE t.webServer.name = :webServerName AND t.templateName = :templateName"),
+                query = "UPDATE JpaWebServerConfigTemplate t SET t.templateContent = :templateContent WHERE " +
+                        "LOWER(t.webServer.name) = LOWER(:webServerName) AND t.templateName = :templateName"),
         @NamedQuery(name = JpaWebServerConfigTemplate.UPDATE_WEBSERVER_TEMPLATE_META_DATA,
-                query = "UPDATE JpaWebServerConfigTemplate t SET t.metaData= :metaData WHERE t.webServer.name = :webServerName AND t.templateName = :templateName"),
+                query = "UPDATE JpaWebServerConfigTemplate t SET t.metaData= :metaData WHERE LOWER(t.webServer.name) " +
+                        "= LOWER(:webServerName) AND t.templateName = :templateName"),
         @NamedQuery(name = JpaWebServerConfigTemplate.GET_WEBSERVER_TEMPLATE,
-        query = "SELECT t FROM JpaWebServerConfigTemplate t where t.webServer.name = :webServerName and t.templateName = :templateName"),
+        query = "SELECT t FROM JpaWebServerConfigTemplate t where LOWER(t.webServer.name) = LOWER(:webServerName) and" +
+                " t.templateName = :templateName"),
         @NamedQuery(name = JpaWebServerConfigTemplate.QUERY_DELETE_WEB_SERVER_TEMPLATE, query="DELETE FROM JpaWebServerConfigTemplate t WHERE t.templateName = :templateName"),
         @NamedQuery(name = JpaWebServerConfigTemplate.QUERY_DELETE_WEBSERVER_RESOURCE_BY_TEMPLATE_WEBSERVER_NAME,
-                    query="DELETE FROM JpaWebServerConfigTemplate t WHERE t.templateName = :templateName AND t.webServer.name = :webServerName"),
+                    query="DELETE FROM JpaWebServerConfigTemplate t WHERE t.templateName = :templateName AND " +
+                            "LOWER(t.webServer.name) = LOWER(:webServerName)"),
         @NamedQuery(name = JpaWebServerConfigTemplate.QUERY_GET_WEBSERVER_RESOURCE_TEMPLATES,
-                query = "SELECT t FROM JpaWebServerConfigTemplate t WHERE t.webServer.name = :webServerName"),
-        @NamedQuery(name = JpaWebServerConfigTemplate.GET_WEBSERVER_TEMPLATE_RESOURCE_NAME, query = "SELECT t FROM JpaWebServerConfigTemplate t WHERE t.templateName = :templateName AND t.webServer.name = :webServerName"),
+                query = "SELECT t FROM JpaWebServerConfigTemplate t WHERE LOWER(t.webServer.name) = " +
+                        "LOWER(:webServerName)"),
+        @NamedQuery(name = JpaWebServerConfigTemplate.GET_WEBSERVER_TEMPLATE_RESOURCE_NAME, query = "SELECT t FROM " +
+                "JpaWebServerConfigTemplate t WHERE t.templateName = :templateName AND LOWER(t.webServer.name) = " +
+                "LOWER(:webServerName)"),
 
         @NamedQuery(name = JpaWebServerConfigTemplate.QUERY_DELETE_WEBSERVER_RESOURCES_BY_TEMPLATE_NAME_LIST_WEBSERVER_NAME,
-                query="DELETE FROM JpaWebServerConfigTemplate t WHERE t.templateName IN :templateNameList AND t.webServer.name = :webServerName"),
+                query="DELETE FROM JpaWebServerConfigTemplate t WHERE t.templateName IN :templateNameList AND " +
+                        "LOWER(t.webServer.name)= LOWER(:webServerName)"),
         @NamedQuery(name = JpaWebServerConfigTemplate.QUERY_GET_WEBSERVER_RESOURCE,
-                query = "SELECT t FROM JpaWebServerConfigTemplate t WHERE t.templateName = :templateName AND t.webServer.name = :webServerName")
+                query = "SELECT t FROM JpaWebServerConfigTemplate t WHERE t.templateName = :templateName AND " +
+                        "LOWER(t.webServer.name) = LOWER(:webServerName)")
         })
 public class JpaWebServerConfigTemplate extends ConfigTemplate {
     public static final String GET_WEBSERVER_RESOURCE_TEMPLATE_NAMES = "getWebServerResourceTemplateNames";
