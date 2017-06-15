c935bf3ad76a43b4052af1f99ad7758414962901
Jedd Cuison <jedd.cuison@cerner.com>
2017-06-15 04:59:40 -0400

    Implement password scrubber by storing encrypted passwords in a set then comparing them to the log message
    Other changes:
    
    - Remove the "other" or the old password scrubber

---
+++ jwala-services/src/main/java/com/cerner/jwala/service/custom/logging/impl/JvmWinSvcAcctPasswordScrubber.java
@@ -0,0 +1,95 @@
+package com.cerner.jwala.service.custom.logging.impl;
+
+import com.cerner.jwala.common.domain.model.jvm.Jvm;
+import com.cerner.jwala.common.domain.model.ssh.DecryptPassword;
+import com.cerner.jwala.persistence.service.JvmPersistenceService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.spi.LoggingEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+/**
+ * Scrubs JVM Windows service account passwords from the logs
+ *
+ * Created by Jedd Cuison on 6/14/2017
+ */
+@Component
+public class JvmWinSvcAcctPasswordScrubber extends PatternLayout {
+
+    public static final String REPLACEMENT = "********";
+    private final Set<String> toRemove = new HashSet<>();
+    private DecryptPassword decryptor;
+    private final String [] includedMethodsArray = {"getExecRemoteCommandReturnInfo", "runExecCommand"};
+    private final Set<String> includedMethods = new HashSet<>(Arrays.asList(includedMethodsArray));
+
+    public JvmWinSvcAcctPasswordScrubber(final JvmPersistenceService jvmPersistenceService, final DecryptPassword decryptor) {
+        this.decryptor = decryptor;
+
+        // Populate the map of items to remove in the logs
+        final List<Jvm> jvms = jvmPersistenceService.getJvms();
+        for (final Jvm jvm: jvms) {
+            if (StringUtils.isNotEmpty(jvm.getEncryptedPassword())) {
+                toRemove.add(jvm.getEncryptedPassword());
+            }
+        }
+
+        // Attach this layout to all appenders to intercept logging
+        final Enumeration enumeration = Logger.getRootLogger().getAllAppenders();
+        while (enumeration.hasMoreElements()) {
+            ((Appender) enumeration.nextElement()).setLayout(this);
+        }
+    }
+
+    @Override
+    public String format(final LoggingEvent event) {
+
+        // Since this class specifically handles JVM windows service passwords (svc_password) found in the logs,
+        // we filter out events, classes and methods that we know won't log svc_password to minimize the impact
+        // of this layout on the application's performance
+        if (event.getLevel() != Level.DEBUG || isClassExcluded(event) || isMethodExcluded(event)) {
+            return super.format(event);
+        }
+
+        final String msg = event.getMessage().toString();
+        for (final String password : toRemove) {
+            if (StringUtils.isNotEmpty(msg)) {
+                final Throwable throwable =
+                        event.getThrowableInformation() != null ? event.getThrowableInformation().getThrowable() : null;
+
+                final LoggingEvent scrubbedEvent = new LoggingEvent(event.fqnOfCategoryClass, Logger.getLogger(
+                        event.getLoggerName()), event.timeStamp, event.getLevel(), msg.replaceAll(
+                        decryptor.decrypt(password), REPLACEMENT), throwable);
+
+                // This scrubber was intended for JVM Windows service installation therefore we can assume that
+                // an event only contains 1 sensitive item to be scrubbed
+                // NOTE: Making this class generic was avoided for performance considerations
+                return super.format(scrubbedEvent);
+            }
+        }
+        return super.format(event);
+    }
+
+    /**
+     * Checks if event class is excluded from svc_password masking
+     * @param event the logging event
+     * @return true if the event class is excluded from processing
+     */
+    private boolean isClassExcluded(final LoggingEvent event) {
+        return !"JschServiceImpl".equalsIgnoreCase(event.getLocationInformation().getClassName());
+    }
+
+    /**
+     * Checks if event method is excluded from svc_password masking
+     * @param event the logging event
+     * @return true if the event method is excluded from processing
+     */
+    private boolean isMethodExcluded(final LoggingEvent event) {
+        return !includedMethods.isEmpty() && !includedMethods.contains(event.getLocationInformation().getMethodName());
+    }
+
+}
