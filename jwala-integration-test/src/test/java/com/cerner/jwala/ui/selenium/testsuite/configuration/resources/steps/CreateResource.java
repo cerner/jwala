f820742661257ccfe2f91a4d8a4f227761e368fe
Jedd Cuison <jedd.cuison@cerner.com>
2017-06-15 14:11:07 -0400

    Fix hashtag not accepted for resource name (#131)
    * Fix hashtag not accepted for resource name
    
    * Add selenium test
    
    Details:
    
    - test resource creation for a resource with hashtag in its name
    - implement test with Cucumber

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/configuration/resources/steps/CreateResource.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/testsuite/configuration/resources/steps/CreateResource.java
@@ -1,24 +1,24 @@
-package com.cerner.jwala.ui.selenium.testsuite.configuration.resources;
+package com.cerner.jwala.ui.selenium.testsuite.configuration.resources.steps;
 
 import com.cerner.jwala.ui.selenium.testsuite.JwalaTest;
-import org.junit.Test;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
- * Replaced by {@link ResourceTest}
- *
- * Created by Jedd Cuison on 3/1/2017
+ * Created by Jedd Cuison on 6/13/2017
  */
-@Deprecated
-public class UploadResourceTest extends JwalaTest {
+public class CreateResource extends JwalaTest {
 
     private static final String GROUP_NAME_2 = "zGroup2-" + CURRENT_TIME_MILLIS;
+    private static final String RESOURCE_NAME_WITH_HASHTAG = "jwala-logo#2";
 
-    @Test
-    public void testUploadResource() {
+    @Given("^Hashtag in the resource name$")
+    public void hashTagInTheResourceName() {
         clickTab("Configuration");
         clickTab("Resources");
         clickTreeItemExpandCollapseIcon(GROUP_NAME_2);
@@ -26,15 +26,22 @@ public class UploadResourceTest extends JwalaTest {
         new WebDriverWait(driver, 5).until(ExpectedConditions.numberOfElementsToBe(
                 By.xpath("//li[span[text()='" + GROUP_NAME_2 + "']]//span[text()='JVMs' and contains(@class, 'ui-state-active')]"), 1));
         driver.findElement(By.xpath("//li[contains(@class, 'button')]/span[@title='create']")).click();
-        driver.switchTo().activeElement().sendKeys("jwala-logo");
+        driver.switchTo().activeElement().sendKeys(RESOURCE_NAME_WITH_HASHTAG);
         driver.switchTo().activeElement().sendKeys(Keys.TAB);
         driver.switchTo().activeElement().sendKeys("/jwala");
         driver.switchTo().activeElement().sendKeys(Keys.TAB);
         driver.switchTo().activeElement()
                 .sendKeys(this.getClass().getClassLoader().getResource("selenium/jwala-logo.png").getPath().replaceFirst("/", ""));
-        driver.findElement(By.xpath("//button[span[text()='Ok']]")).click();
+    }
 
-        testForBusyIcon(3, 10);
+    @When("^Resource is created$")
+    public void resourceCreated() {
+        driver.findElement(By.xpath("//button[span[text()='Ok']]")).click();
+    }
+
+    @Then("^Resource creation is successful$")
+    public void resourceCreatedSuccessfully() {
+        waitUntilElementIsVisible(By.xpath("//span[text()='" + RESOURCE_NAME_WITH_HASHTAG +  "']"));
     }
 
 }
---
+++ jwala-integration-test/src/test/resources/com/cerner/jwala/ui/selenium/testsuite/configuration/resources/addResource.feature
@@ -0,0 +1,7 @@
+Feature: CreateResource
+
+Scenario: Create a resource file with hashtag
+
+Given Hashtag in the resource name
+When Resource is created
+Then Resource creation is successful
