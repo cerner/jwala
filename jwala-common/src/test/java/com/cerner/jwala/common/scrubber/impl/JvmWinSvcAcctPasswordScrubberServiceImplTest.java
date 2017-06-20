298bb147ae7a6d81b70bb62302f09464fff9686e
Jedd Cuison <jedd.cuison@cerner.com>
2017-06-20 19:22:40 -0400

    Change and add test strings that reflect the real ones

--- jwala-common/src/test/java/com/cerner/jwala/common/scrubber/impl/JvmWinSvcAcctPasswordScrubberServiceImplTest.java
+++ jwala-common/src/test/java/com/cerner/jwala/common/scrubber/impl/JvmWinSvcAcctPasswordScrubberServiceImplTest.java
@@ -17,6 +17,13 @@ public class JvmWinSvcAcctPasswordScrubberServiceImplTest {
 
     private JvmWinSvcAcctPasswordScrubberServiceImpl jvmWinSvcAcctPasswordScrubberService;
 
+    private static String TEST_RAW_STR_1 = "D:/cygwin64/home/someUser>set svc_password=secret ";
+    private static String SCRUBBED_STR_1 = "D:/cygwin64/home/someUser>set svc_password=******** ";
+    private static String TEST_RAW_STR_2 = "Executing command \"~/.jwala/SOME-SERVER/install-service.sh SOME-SERVER " +
+            "D:/ctp/app/instances apache-tomcat-7.0.55 \"the-user\" secret \"";
+    private static String SCRUBBED_STR_2 = "Executing command \"~/.jwala/SOME-SERVER/install-service.sh SOME-SERVER " +
+            "D:/ctp/app/instances apache-tomcat-7.0.55 \"the-user\" ******** \"";
+
     @Mock
     private DecryptPassword mockDecryptPassword;
 
@@ -29,9 +36,16 @@ public class JvmWinSvcAcctPasswordScrubberServiceImplTest {
     }
 
     @Test
-    public void testScrub() throws Exception {
+    public void testScrubRawStr1() {
         when(mockDecryptPassword.decrypt("secret")).thenReturn("secret");
-        final String result = jvmWinSvcAcctPasswordScrubberService.scrub("password = secret");
-        assertEquals("password = ********", result);
+        final String result = jvmWinSvcAcctPasswordScrubberService.scrub(TEST_RAW_STR_1);
+        assertEquals(SCRUBBED_STR_1, result);
+    }
+
+    @Test
+    public void testScrubRawStr2() {
+        when(mockDecryptPassword.decrypt("secret")).thenReturn("secret");
+        final String result = jvmWinSvcAcctPasswordScrubberService.scrub(TEST_RAW_STR_2);
+        assertEquals(SCRUBBED_STR_2, result);
     }
 }
