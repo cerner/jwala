e062ebee833f34ba209eb82e92c3948ad62841ef
Sharvari Barve <Sharvari.barve@cerner.com>
2017-08-08 15:11:02 -0400

    Update HttpdConfRunSteps.java

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/operations/HttpdConfRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/operations/HttpdConfRunSteps.java
@@ -12,16 +12,14 @@ public class HttpdConfRunSteps {
     @Autowired
     JwalaUi jwalaUi;
 
-
     @Then("^I see the proper httpd.conf")
     public void verifyConf() {
         jwalaUi.isElementExists(By.xpath("/contains(text(),'This is the main Apache HTTP server configuration file.')"));
-
     }
 
     @Then("^I see error while reading conf file")
     public void verifyError() {
         jwalaUi.isElementExists(By.xpath("/contains(text(),'Error reading httpd.conf:')"));
-
     }
+    
 }
