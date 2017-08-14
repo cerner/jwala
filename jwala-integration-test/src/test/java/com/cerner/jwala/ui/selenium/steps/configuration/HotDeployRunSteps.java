c7e4abf22d2577c2af02a30bf24d352e55deae0b
Barve <SB053052@cerner.net>
2017-08-14 10:36:39 -0400

    Reformatted code and changed according to cucumber standards

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/HotDeployRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/HotDeployRunSteps.java
@@ -17,38 +17,35 @@ public class HotDeployRunSteps {
     @Autowired
     JwalaUi jwalaUi;
 
-    @When("I click \"(.*)\" tab$")
+    @When("^I click \"(.*)\" tab$")
     public void clickTab(String text) {
         jwalaUi.click(By.xpath("//a[text()='" + text + "']"));
     }
 
-
-    @When("I select resource file \"(.*)\"$")
+    @When("^I select resource file \"(.*)\"$")
     public void selectFile(String fileName) {
         jwalaUi.click(By.xpath("//span[text()='" + fileName + "']"));
     }
 
-    @When("I right click resource file \"(.*)\"$")
+    @When("^I right click resource file \"(.*)\"$")
     public void rightClickFile(String fileName) {
         jwalaUi.rightClick(By.xpath("//span[text()='" + fileName + "']"));
     }
 
-    @When("I click deploy option$")
-    public void clickDeploy(){
+    @When("^I click deploy option$")
+    public void clickDeploy() {
         jwalaUi.click(By.xpath("//*[text()='deploy']"));
     }
 
-    @When("I click deploy to a host option$")
-    public void clickDeployToAHost(){
+    @When("^I click deploy to a host option$")
+    public void clickDeployToAHost() {
         jwalaUi.click(By.xpath("//*[text()='a host']"));
     }
 
-    @When("I click deploy All option$")
-    public void clickDeployAll(){
+    @When("^I click deploy All option$")
+    public void clickDeployAll() {
         jwalaUi.click(By.xpath("//*[text()='all hosts']"));
     }
-
-
 
     @When("^I enter hot Deploy value \"(.*)\" in the edit box for the file \"(.*)\"$")
     public void checkForHotDeploy(String text, String file) {
@@ -65,7 +62,7 @@ public class HotDeployRunSteps {
             jwalaUi.sendKeys(Keys.ENTER);
             jwalaUi.sendKeys(text + ",");
         } else {
-            WebElement hotDeployElement =jwalaUi.getWebElement(By.xpath("//span[contains(text(), 'hotDeploy')]"));
+            WebElement hotDeployElement = jwalaUi.getWebElement(By.xpath("//span[contains(text(), 'hotDeploy')]"));
             hotDeployElement.findElement(By.xpath("..")).findElement(By.xpath("..")).click();
             int i = 5;
             while (i >= 0) {
@@ -75,7 +72,6 @@ public class HotDeployRunSteps {
             String value = "true";
             jwalaUi.sendKeys(value);
             jwalaUi.sendKeys(Keys.ENTER);
-
 
         }
     }
@@ -88,25 +84,26 @@ public class HotDeployRunSteps {
         if (elements.size() > 0) {
             WebElement finalElement = elements.get(0);
         }
-            WebElement jvmElement =jwalaUi.getWebElement(By.xpath("//span[contains(text(), 'deployToJvms')]"));
-            jvmElement.findElement(By.xpath("..")).findElement(By.xpath("..")).click();
-            int i = 5;
-            while (i >= 0) {
-                jwalaUi.sendKeys(Keys.BACK_SPACE);
-                i--;
-            }
-            String value = "true";
-            jwalaUi.sendKeys(value);
-            jwalaUi.sendKeys(Keys.ENTER);
+        WebElement jvmElement = jwalaUi.getWebElement(By.xpath("//span[contains(text(), 'deployToJvms')]"));
+        jvmElement.findElement(By.xpath("..")).findElement(By.xpath("..")).click();
+        int i = 5;
+        while (i >= 0) {
+            jwalaUi.sendKeys(Keys.BACK_SPACE);
+            i--;
+        }
+        String value = "true";
+        jwalaUi.sendKeys(value);
+        jwalaUi.sendKeys(Keys.ENTER);
 
     }
 
-    @Then("I verify successful deploy$")
-    public void verifySuccesfulDeploy(){
+    @Then("^I verify successful deploy$")
+    public void verifySuccesfulDeploy() {
         jwalaUi.waitUntilElementIsVisible(By.xpath("//*[contains(text(),'Deploy successful!')]"));
     }
-    @Then("I verify error message for file \"(.*)\" for jvm \"(.*)\"$")
-    public void verifyErrorMessageIndividiualJvm(String file, String jvm){
-        jwalaUi.waitUntilElementIsVisible(By.xpath("//*[contains(text(),'The target JVM "+jvm+" must be stopped or the resource "+file+" must be set to hotDeploy=true before attempting to update the resource files')]"));
+
+    @Then("^I verify error message for file \"(.*)\" for jvm \"(.*)\"$")
+    public void verifyErrorMessageIndividiualJvm(String file, String jvm) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//*[contains(text(),'The target JVM " + jvm + " must be stopped or the resource " + file + " must be set to hotDeploy=true before attempting to update the resource files')]"));
     }
 }
