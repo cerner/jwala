53fc943a3b9a4cf24575b51a542413c029672a1e
Barve <SB053052@cerner.net>
2017-08-14 15:45:09 -0400

    Added Scenarios

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/HotDeployRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/HotDeployRunSteps.java
@@ -106,4 +106,24 @@ public class HotDeployRunSteps {
     public void verifyErrorMessageIndividiualJvm(String file, String jvm) {
         jwalaUi.waitUntilElementIsVisible(By.xpath("//*[contains(text(),'The target JVM " + jvm + " must be stopped or the resource " + file + " must be set to hotDeploy=true before attempting to update the resource files')]"));
     }
+
+    @Then("^I verify error message for ws file \"(.*)\" for webserver \"(.*)\"$")
+    public void verifyErrorMessageIndividiualWebServer(String file, String webserver) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//*[contains(text(),'The target Web Server " + webserver + " must be stopped or the resource must be configured to be hotDeploy=true before attempting to deploy the resource " + file + "')]"));
+    }
+
+    @Then("^I verify error message for group \"(.*)\" for jvm file \"(.*)\" with one of JVMs as \"(.*)\"$")
+    public void verifyErrorMessageGroupJvm(String group, String file, String jvm) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//*[contains(text(),'Failed to deploy file " + file + " for group " + group + " since the following JVMs are running and the file is not configured for hot deploy: [" + jvm + "]')]"));
+    }
+
+    @Then("^I verify ws error message for group \"(.*)\" for ws file \"(.*)\" with one of Web-servers as \"(.*)\"$")
+    public void verifyErrorMessageGroupWebServer(String group, String file, String ws) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//div[contains(text(),'Failed to deploy " + file + " for group " + group + ": the following Web Servers were started and the resource was not configured for hotDeploy=true: [" + ws + "]')]"));
+    }
+
+    @Then("I verify error message for group \"(.*)\" for app file \"(.*)\" with one of JVMs as \"(.*)\"$")
+    public void verifyErrorMessageIndividualApp(String group, String file, String jvm) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//div[contains(text(),'Failed to deploy file " + file + " for group " + group + ": not all JVMs were stopped - the following JVMs were started and the resource was not configured with hotDeploy=true: [" + jvm + "]')]"));
+    }
 }
