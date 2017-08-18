173de4262442223b5594bbe4fbc2acdc766fe59c
Jedd Cuison <jedd.cuison@cerner.com>
2017-08-18 15:59:48 -0400

    Transfer to operation package and remove @After

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/operation/StatusRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/operation/StatusRunSteps.java
@@ -1,26 +1,19 @@
-package com.cerner.jwala.ui.selenium.steps.operations;
+package com.cerner.jwala.ui.selenium.steps.operation;
 
-import com.cerner.jwala.ui.selenium.SeleniumTestHelper;
 import com.cerner.jwala.ui.selenium.component.JwalaUi;
-import com.cerner.jwala.ui.selenium.steps.CommonRunSteps;
-import cucumber.api.java.After;
-import cucumber.api.java.cs.A;
 import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.Properties;
-
-import static junit.framework.TestCase.assertNotNull;
 
 /**
  * Created by Sharvari Barve on 7/18/2017.
  */
 public class StatusRunSteps {
+
     @Autowired
     private JwalaUi jwalaUi;
 
@@ -33,10 +26,5 @@ public class StatusRunSteps {
         String hostName = paramProp.getProperty(host);
         jwalaUi.isElementExists(By.xpath("//td[text()='http://" + hostName + ":" + port + "/" + app + "')"));
         jwalaUi.isElementExists(By.xpath("//h1[text()='Load Balancer Manager for" + hostName + "'"));
-    }
-
-    @After
-    public void afterScenario() throws SQLException, IOException, ClassNotFoundException {
-        SeleniumTestHelper.runSqlScript(this.getClass().getClassLoader().getResource("./selenium/cleanup.sql").getPath());
     }
 }
