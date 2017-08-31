fc0da570446ea930f612628755f1470c23084959
Jedd Cuison <jedd.cuison@cerner.com>
2017-08-31 14:11:26 -0400

    Fix failing test in Jenkins and improve it as well

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/admin/AdminTest.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/admin/AdminTest.java
@@ -10,5 +10,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"classpath:com/cerner/jwala/ui/selenium/admin/admin.feature" },
-        glue = {"com.cerner.jwala.ui.selenium.steps"})
+        glue = {"com.cerner.jwala.ui.selenium.steps"}, strict = true)
 public class AdminTest {}
--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/admin/AdminRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/admin/AdminRunSteps.java
@@ -2,17 +2,15 @@ package com.cerner.jwala.ui.selenium.steps.admin;
 
 import com.cerner.jwala.ui.selenium.component.JwalaUi;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.Properties;
-
-import static junit.framework.TestCase.assertNotNull;
 
 /**
  * Created by Sharvari Barve on 7/21/2017.
@@ -26,53 +24,30 @@ public class AdminRunSteps {
     @Qualifier("parameterProperties")
     private Properties paramProp;
 
-    @Given("^I am in the admin tab$")
-    public void goToAdminTab() {
-        jwalaUi.clickTab("Admin");
-    }
-
-    @When("^I fill in the data to be encrypted \"(.*)\"$")
+    @When("^I fill in the \"data to be secured\" field with \"(.*)\"$")
     public void fillDataToEncrypt(String data) {
         jwalaUi.sendKeys(By.xpath("//input[@class='toEncrypt']"), data);
     }
 
-    @And("^I click \"(.*)\" button$")
-    public void clickEncryptButton(String componentName) {
-        jwalaUi.click(By.xpath("//button/span[contains(text(),'" + componentName + "')]"));
+    @And("^I click the admin's tab \"(.*)\" button$")
+    public void clickEncryptBtn(final String btnLabel) {
+        jwalaUi.click(By.xpath("//button/span[text() = '" + btnLabel + "']"));
     }
 
-    @Then("^I see the text \"(.*)\" in properties management box$")
-    public void verifyPropertiesManagementTextBox(String text) {
-        WebElement element = jwalaUi.getWebElement(By.xpath("//p[8]/textarea"));
-        String textInTheBox = element.getAttribute("value");
-        assertNotNull(textInTheBox);
-        assert textInTheBox.contains(text);
-    }
-
-    @Then("^I see the text \"(.*)\" in manifest box$")
-    public void verifyManifestTextBox(String text) {
-        WebElement element = jwalaUi.getWebElement(By.xpath("//p[10]/textarea"));
-        String textInTheBox = element.getAttribute("value");
-        assertNotNull(textInTheBox);
-        assert textInTheBox.contains(text);
-    }
-
-    @And("^I see the implementation version \"(.*)\" in manifest box$")
-    public void verifyVersion(String text) {
-        WebElement element = jwalaUi.getWebElement(By.xpath("//p[10]/textarea"));
-        String textInTheBox = element.getAttribute("value");
-        assertNotNull(textInTheBox);
-        assert textInTheBox.contains(paramProp.getProperty(text));
-    }
-
-    @And("^I verify header with text \"(.*)\"$")
-    public void verifyHeader(String text) {
-        WebElement header = jwalaUi.getWebElement(By.xpath("//h3[contains(text(),'" + text + "')]"));
-        assertNotNull(header);
-    }
-
-    @And("^I verify succesful Encryption message$")
+    @Then("^I see the \"Encryption Succeeded\" message$")
     public void verifyText() {
-        jwalaUi.isElementExists(By.xpath("//p[contains(text(),'Encryption Succeeded']"));
+        jwalaUi.find(By.xpath("//h4[text()='Encryption Succeeded']"));
+    }
+
+    @And("^I see the \"(.*)\" heading$")
+    public void verifyHeader(final String headerCaption) {
+        jwalaUi.find(By.xpath("//h3[text()='" + headerCaption + "']"));
+    }
+
+    @And("^I see \"(.*)\" in the \"(.*)\" text box$")
+    public void verifyTextAreaItem(final String itemName, final String textAreaHeading) {
+        final WebElement webElement = jwalaUi.find(By.xpath("//h3[text()='" + textAreaHeading + "']/following-sibling::p//textArea"));
+        final String val = webElement.getAttribute("value");
+        assert StringUtils.isNotEmpty(val) && val.contains(itemName);
     }
 }
