d55241681eea7103b8226ce98953381117610971
Barve <SB053052@cerner.net>
2017-09-02 23:47:04 -0400

    Added general methods for resources and external properties

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/ControlResourcesRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/ControlResourcesRunSteps.java
@@ -1,7 +1,106 @@
 package com.cerner.jwala.ui.selenium.steps.configuration;
 
+import com.cerner.jwala.ui.selenium.component.JwalaUi;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.assertFalse;
+
 /**
  * Created by SB053052 on 9/2/2017.
  */
 public class ControlResourcesRunSteps {
+
+    @Autowired
+    private JwalaUi jwalaUi;
+
+    @Autowired
+    @Qualifier("parameterProperties")
+    private Properties paramProp;
+
+    @When("^I click \"(.*)\" component$")
+    public void selectComponent(String component) {
+        jwalaUi.clickWhenReady(By.xpath("//span[contains(text(),'" + component + "')]"));
+    }
+
+    @And("^I select the resource file \"([^\"]*)\"$")
+    public void clickResource(String resource) {
+        jwalaUi.click(By.xpath("//span[text()='" + resource + "']"));
+    }
+
+    @Then("^I verify external property override message$")
+    public void verifyOverrideMsg() {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//span[text()='Only one external properties file can be uploaded. Any existing ones will be overwritten.']"));
+    }
+
+    @When("^I expand \"(.*)\" node in data tree$")
+    public void expandPropertyDataTree(String property) {
+        jwalaUi.click(By.xpath("//span[contains(@class,'nodeKey') and contains(text(),'" + property + "') ]/preceding-sibling::span"));
+    }
+
+    @Then("^I verify dataTree has the key-value pair as \"(.*)\" and \"(.*)\" respectively$")
+    public void verifyDataTree(String key, String value) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//span[contains(@class,'nodeKey') and contains(text(),'" + key + "') ]"));
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//span[contains(@class,'nodeVal') and contains(text(),'" + value + "')]"));
+    }
+
+    @When("^I add property \"(.*)\"$")
+    public void addProperty(String property) {
+        jwalaUi.click(By.xpath("//div[contains(@class, 'CodeMirror') and contains(@class, 'cm-s-default')]"));
+        jwalaUi.sendKeys(Keys.ENTER + property + Keys.ENTER);
+    }
+
+
+    @When("^I enter value \"(.*)\" in the edit box for the file \"(.*)\"$")
+    public void enterInEditBox(String text, String file) {
+        jwalaUi.click(By.xpath("//*[text()='{']"));
+        jwalaUi.sendKeys(Keys.DELETE);
+        jwalaUi.sendKeys("{");
+        jwalaUi.sendKeys(Keys.ENTER);
+        jwalaUi.sendKeys(text + ",");
+    }
+
+    @When("^I click save button of edit box of \"(.*)\"$")
+    public void saveButton(String tab) {
+        List<WebElement> elements = jwalaUi.getWebDriver().findElements(By.xpath("//span[contains(@class, 'ui-icon-disk') and @title='Save']"));
+        WebElement saveElement = tab.equals("Template") ? elements.get(0) : elements.get(1);
+        saveElement.click();
+    }
+
+    @When("^I wait for  notification \"(.*)\"")
+    public void waitForNotification(String notification) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//div[text()='" + notification + "']"));
+    }
+
+    @Then("^I verify edit \"(.*)\"$")
+    public void verifyText(String text) {
+        jwalaUi.waitUntilElementIsVisible(By.xpath("//div[contains(.,'" + text + "')]"));
+    }
+
+    @When("^I click the resource delete icon$")
+    public void clickDelete() {
+        jwalaUi.click(By.xpath("//*[@title='delete']"));
+    }
+
+    @When("^I click check-box for resourceFile \"(.*)\"$")
+    public void selectResourceCheckBox(String resourceFile) {
+        jwalaUi.clickWhenReady(By.xpath("//li/span[text()='" + resourceFile + "']/preceding-sibling::input[@type='checkbox']"));
+    }
+
+    @And("^I click yes button to delete a resource$")
+    public void deleteResource() {
+        jwalaUi.click(By.xpath("//button/span[text()='Yes']"));
+    }
+
+
 }
