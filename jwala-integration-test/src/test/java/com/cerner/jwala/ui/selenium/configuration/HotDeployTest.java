96ba5a8a6258e0e449201d99ccfd85e9b6e38b30
Barve <SB053052@cerner.net>
2017-09-04 16:31:23 -0400

    Changed feature file name

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/configuration/HotDeployTest.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/configuration/HotDeployTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"classpath:com/cerner/jwala/ui/selenium/configuration/externalProperties.feature"},
+        features = {"classpath:com/cerner/jwala/ui/selenium/configuration/hotDeploy.feature"},
         glue = {"com.cerner.jwala.ui.selenium.steps"})
 public class HotDeployTest {
 }
