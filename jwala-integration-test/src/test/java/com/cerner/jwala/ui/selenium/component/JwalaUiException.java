139523805d9640627bbe3cb101749ae42b0396bd
Jedd Cuison <jedd.cuison@cerner.com>
2017-10-03 16:15:23 -0400

    Add screenShot method

---
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/component/JwalaUiException.java
@@ -0,0 +1,11 @@
+package com.cerner.jwala.ui.selenium.component;
+
+/**
+ * Created by Jedd Cuison on 10/3/2017
+ */
+public class JwalaUiException extends RuntimeException {
+
+    public JwalaUiException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
