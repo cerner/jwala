e8e68ed3c6379a7a56b12a66aa75960655eae1cb
Jedd Cuison <jedd.cuison@cerner.com>
2017-10-04 11:07:51 -0400

    Fix for failing UI integration tests in Jenkins (#168)
    * Add ignore tag
    
    * Simplify Cucumber string parameter placeholder
    
    * Wait for the edit box to appear before sending text to it
    
    * Fix bug "Element is not clickable at point..."
    
    * Use "Then"
    
    * Update java doc
    
    * Fix intermittent issue "Element is not clickable at point..."
    
    * Refactor scroll into view into its own method
    
    * Use visibilityOfElementLocated
    
    * Remove click from scrollIntoView method

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/configuration/ExternalPropertiesTest.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/configuration/ExternalPropertiesTest.java
@@ -10,6 +10,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"classpath:com/cerner/jwala/ui/selenium/configuration/externalProperties.feature"},
-        glue = {"com.cerner.jwala.ui.selenium.steps"}, strict = true)
-public class ExternalPropertiesTest {
-}
+        glue = {"com.cerner.jwala.ui.selenium.steps"}, tags = {"~@ignore"}, strict = true)
+public class ExternalPropertiesTest {}
