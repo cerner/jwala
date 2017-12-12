a506859610ec091d3650ebd6a25929fcd1d9657a
Barve <SB053052@cerner.net>
2017-12-11 21:11:41 -0500

    changed the script to save reports as files and added test

--- jwala-tomcat/src/main/resources/data/scripts/thread-dump.sh
+++ jwala-tomcat/src/main/resources/data/scripts/thread-dump.sh
@@ -8,12 +8,19 @@ esac
 export JAVA_HOME=$1
 export JVM_INSTANCE_DIR=$2
 export JVM_NAME=$3
+
+if [ ! -d ${JVM_INSTANCE_DIR}/thread_dump_reports} ]; then
+      mkdir -p ${JVM_INSTANCE_DIR}/thread_dump_reports
+fi
+
+current_time=$(date +"%m-%d-%Y-%T")
+
 if $linux; then
 	echo $(<${JVM_INSTANCE_DIR}/logs/catalina.pid)
-	/usr/bin/sudo -u tomcat ${JAVA_HOME}/bin/jstack $(<${JVM_INSTANCE_DIR}/logs/catalina.pid)
+	/usr/bin/sudo -u tomcat ${JAVA_HOME}/bin/jstack $(<${JVM_INSTANCE_DIR}/logs/catalina.pid) 2>&1 | tee  ${JVM_INSTANCE_DIR}/thread_dump_reports/thread_dump_$current_time
 fi
 
 if $cygwin; then
     export JVMPID=`sc queryex $JVM_NAME | /usr/bin/grep PID | /usr/bin/awk '{ print $3 }'`
-	${JAVA_HOME}/bin/jstack -l ${JVMPID}
+	${JAVA_HOME}/bin/jstack -l ${JVMPID} 2>&1 |tee ${JVM_INSTANCE_DIR}/thread_dump_reports/thread_dump_$current_time
 fi
