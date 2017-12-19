95d6d02792464370c067bdb34c5a218670d1cef3
Barve <SB053052@cerner.net>
2017-12-18 21:02:13 -0500

    removed sleep

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/component/JwalaUi.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/component/JwalaUi.java
@@ -58,7 +58,6 @@ public class JwalaUi {
         webDriverWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".AppBusyScreen")));
         final WebElement webElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(by));
         scrollIntoView(webElement);
-        sleep();
         webElement.click();
     }
 
@@ -181,7 +180,6 @@ public class JwalaUi {
     public void click(final By by) {
         final WebElement webElement = driver.findElement(by);
         scrollIntoView(webElement);
-        sleep();
         webElement.click();
     }
 
