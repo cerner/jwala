13dbdb9b06ff32eb02660bf2fd314ea88541dc09
jmahmoodcc <Jeffrey.Mahmood@cerner.com>
2017-12-29 10:44:49 -0500

    add swagger annotations to UserServiceRest

--- jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/user/UserServiceRest.java
+++ jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/user/UserServiceRest.java
@@ -1,36 +1,42 @@
 package com.cerner.jwala.ws.rest.v1.service.user;
 
+import io.swagger.annotations.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
-
+@Api(value = "/user", tags = "user")
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
 public interface UserServiceRest {
 
     @POST
     @Path("/login")
+    @ApiOperation(value = "Login to the application",
+            response = Response.class
+    )
+    @ApiResponses(@ApiResponse(code = 500, message = "Error login"))
     Response login(@Context HttpServletRequest request,
-                   @FormParam("userName") String userName,
-                   @FormParam("password") String password);
+                   @ApiParam(value = "The user", required = true) @FormParam("userName") String userName,
+                   @ApiParam(value = "The user's password", required = true) @FormParam("password") String password);
 
     @POST
     @Path("/logout")
-    Response logout(@Context HttpServletRequest request, 
+    @ApiOperation(value = "Logout from the application",
+            response = Response.class
+    )
+    Response logout(@Context HttpServletRequest request,
                     @Context HttpServletResponse response);
     
     @GET
     @Path("/isUserAdmin")
-    Response isUserAdmin(@Context HttpServletRequest request, 
+    @ApiOperation(value = "Checks if the current logged on user is an administrator for the application",
+            response = String.class
+    )
+    Response isUserAdmin(@Context HttpServletRequest request,
                          @Context HttpServletResponse response);
 }
