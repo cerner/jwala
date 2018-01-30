d34cc4d56b870cc50d3d56278a2e25debf2c09d6
Barve <SB053052@cerner.net>
2018-01-30 11:08:04 -0500

    reintroduced code for selenium linux service

---
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/JwalaOsType.java
@@ -0,0 +1,8 @@
+package com.cerner.jwala.ui.selenium.steps;
+
+/**
+ * Created by Jedd Cuison on 9/19/2017
+ */
+public enum JwalaOsType {
+    WINDOWS, UNIX
+}
--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/TearDownStep.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/TearDownStep.java
@@ -5,13 +5,18 @@ import static com.cerner.jwala.common.domain.model.jvm.JvmState.*;
 
 import com.cerner.jwala.common.exec.RemoteSystemConnection;
 import com.cerner.jwala.common.jsch.JschService;
+import com.cerner.jwala.common.jsch.RemoteCommandReturnInfo;
 import com.cerner.jwala.ui.selenium.SeleniumTestHelper;
 import cucumber.api.java.After;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.io.IOException;
 import java.sql.*;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -35,6 +40,8 @@ public class TearDownStep {
     final String userName;
     final String password;
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(TearDownStep.class);
+
     public TearDownStep() throws IOException, ClassNotFoundException {
         properties = SeleniumTestHelper.getProperties();
         Class.forName(properties.getProperty("jwala.db.driver"));
@@ -52,9 +59,14 @@ public class TearDownStep {
         final List<ServiceInfo> webServerNameList;
         final List<ServiceInfo> jvmNameList;
 
+        LOGGER.info("Running after scenario...");
+
         try {
             webServerNameList = getWebServers();
             jvmNameList = getJvms();
+
+            LOGGER.info("Web server name list size = {}", webServerNameList.size());
+            LOGGER.info("JVM name list size = {}", jvmNameList.size());
         } catch (final IOException | ClassNotFoundException | SQLException e) {
             throw new TearDownException("There was an error in retrieving the list of web servers from the database!", e);
         }
@@ -63,10 +75,12 @@ public class TearDownStep {
         final String sshPwd = props.getProperty("ssh.user.pwd");
 
         for (final ServiceInfo serviceInfo : webServerNameList) {
+            LOGGER.info("deleting web server \"{}\" service...", serviceInfo.name);
             deleteService(sshUser, sshPwd, serviceInfo);
-    }
+        }
 
         for (final ServiceInfo serviceInfo : jvmNameList) {
+            LOGGER.info("deleting JVM \"{}\" service...", serviceInfo.name);
             deleteService(sshUser, sshPwd, serviceInfo);
         }
 
@@ -83,15 +97,44 @@ public class TearDownStep {
         final RemoteSystemConnection remoteSystemConnection
                 = new RemoteSystemConnection(sshUser, sshPwd, serviceInfo.host, 22);
 
+        RemoteCommandReturnInfo remoteCommandReturnInfo
+                = jschService.runShellCommand(remoteSystemConnection, "uname", 10000);
+        LOGGER.info("Executed uname, result = {}", remoteCommandReturnInfo.standardOuput);
+        final JwalaOsType osType = StringUtils.indexOf(remoteCommandReturnInfo.standardOuput, "CYGWIN") > -1 ?
+                JwalaOsType.WINDOWS : JwalaOsType.UNIX;
+        LOGGER.info("OS type = {}", osType);
+
         if (serviceInfo.isStarted()) {
             // Stop the service first so that service delete is executed by the OS asap
-            jschService.runShellCommand(remoteSystemConnection, "sc stop " + serviceInfo.name, 300000);
+            switch (osType) {
+                case WINDOWS:
+                    remoteCommandReturnInfo = jschService.runShellCommand(remoteSystemConnection, "sc stop " + serviceInfo.name, 300000);
+                    break;
+                case UNIX:
+                    remoteCommandReturnInfo = jschService.runShellCommand(remoteSystemConnection, "sudo service " + serviceInfo.name + " stop", 300000);
+                    break;
+                default:
+                    throw new TearDownException(
+                            MessageFormat.format("I don't know how to stop services for os type -> {0}", osType));
+            }
+            LOGGER.info("Shell command executed = {}", remoteCommandReturnInfo.standardOuput);
         }
 
         // If the service state is NEW, that means that the service has not yet been created so let's not issue a
         // delete command to save some time
         if (!serviceInfo.isNew()) {
-            jschService.runShellCommand(remoteSystemConnection, "sc delete " + serviceInfo.name, 300000);
+            switch (osType) {
+                case WINDOWS:
+                    remoteCommandReturnInfo = jschService.runShellCommand(remoteSystemConnection, "sc delete " + serviceInfo.name, 300000);
+                    break;
+                case UNIX:
+                    remoteCommandReturnInfo = jschService.runShellCommand(remoteSystemConnection, "sudo rm /etc/init.d/" + serviceInfo.name, 300000);
+                    break;
+                default:
+                    throw new TearDownException(
+                            MessageFormat.format("I don't know how to delete services for os type -> {0}", osType));
+            }
+            LOGGER.info("Shell command executed = {}", remoteCommandReturnInfo.standardOuput);
         }
     }
 
