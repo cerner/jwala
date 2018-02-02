1423878142974aa8873c47367d5e770787446be4
Barve <SB053052@cerner.net>
2018-02-02 14:47:06 -0500

    added desired capability for chrome

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/SeleniumTestHelper.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/SeleniumTestHelper.java
@@ -57,6 +57,9 @@ public class SeleniumTestHelper {
             dc.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, true);
             dc.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
         }
+        else if(webDriverClass.equalsIgnoreCase(ORG_OPENQA_SELENIUM_IE_INTERNET_EXPLORER_DRIVER)) {
+            dc = DesiredCapabilities.chrome();
+        }
 
         final String hubUrl;
         try {
