427de652315b1011252f2d135d1b240fe5074f56
Jeffrey Mahmood <Jeffrey.Mahmood@cerner.com>
2019-03-15 15:28:37 -0400

    add details to error message and remove /usr/bin/ from stop-service.sh

--- jwala-tomcat/src/main/resources/data/scripts/stop-service.sh
+++ jwala-tomcat/src/main/resources/data/scripts/stop-service.sh
@@ -20,45 +20,46 @@ if [ "$1" = "" -o "$2" = "" ]; then
 fi
 
 if $cygwin; then
-  export JVMINST=`sc queryex $1 | /usr/bin/head -1 | /usr/bin/awk '{ sub(/:/,"",$4); print $4 }'`
-  export JVMPID=`sc queryex $1 | /usr/bin/grep PID | /usr/bin/awk '{ print $3 }'`
+  export JVMINST=`sc queryex $1 | head -1 | /usr/bin/awk '{ sub(/:/,"",$4); print $4 }'`
+  export JVMPID=`sc queryex $1 | grep PID | /usr/bin/awk '{ print $3 }'`
   if [ "$JVMINST" = "1060" ]; then
       sc queryex $1
-      /usr/bin/echo Service $1 not installed on server
+      echo Service $1 not installed on server
       exit $JWALA_EXIT_CODE_NO_SUCH_SERVICE
   elif [ "$JVMPID" -ne "0" ]; then
       sc stop $1 > /dev/null
       export SCRETURN=$?
       if [ "$SCRETURN" -ne "0" ]; then
-          /usr/bin/echo `sc stop $1`
+          echo `sc stop $1`
           exit $SCRETURN
       fi
       for (( c=1; c<=$2; c++ ))
       do
-          /usr/bin/sleep 1
-          export JVMNEWPID=`sc queryex  $1 | /usr/bin/grep PID | /usr/bin/awk '{ print $3 }'`
+          sleep 1
+          export JVMNEWPID=`sc queryex  $1 | grep PID | /usr/bin/awk '{ print $3 }'`
           if [ $JVMNEWPID -ne $JVMPID ]; then
-              /usr/bin/echo Successfully stopped service $1
+              echo Successfully stopped service $1
               exit $JWALA_EXIT_CODE_SUCCESS
           fi
       done
-      ( sc query $1 | /usr/bin/tail -8 )
-      /usr/bin/kill -9 -f $JVMPID
-      /usr/bin/echo Service $1 with process id $JVMPID terminated.
+      ( sc query $1 | tail -8 )
+      kill -9 -f $JVMPID
+      echo Service $1 with process id $JVMPID terminated.
       exit $JWALA_EXIT_CODE_KILL
   else
-      /usr/bin/echo The service has not been started.
+      echo The service has not been started.
       exit $JWALA_EXIT_CODE_ABNORMAL_SUCCESS
   fi
 fi
 
 if $linux; then
-  get_version=$(uname -r)
+  os_version=$(uname -r)
   linux_7="el7"
-  if [[ $get_version =~ $linux_7 ]];then
-    /usr/bin/sudo sudo systemctl stop $1
+  if [[ $os_version =~ $linux_7 ]];then
+    sudo sudo systemctl stop $1
   else
-    /usr/bin/echo Linux 6 found
+    echo $os_version found but was expecting $linux_7
+    echo Exiting with ERROR CODE $JWALA_EXIT_CODE_INVALID_OS
     exit $JWALA_EXIT_CODE_INVALID_OS;
   fi
 fi
