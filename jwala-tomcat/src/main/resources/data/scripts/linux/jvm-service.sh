bd8c3f94d2660270209d70493618f5997e6d8920
mg047986 <manisha.gupta3@cerner.com>
2019-03-18 19:06:33 +0530

    code review changes

--- jwala-tomcat/src/main/resources/data/scripts/linux/jvm-service.sh
+++ jwala-tomcat/src/main/resources/data/scripts/linux/jvm-service.sh
@@ -1,38 +1,24 @@
 #!/bin/sh
 
-### BEGIN INIT INFO
-# Provides:          tomcat @JVM_NAME@
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start and stop Apache Tomcat
-# Description:       Enable Apache Tomcat service provided by daemon.
-# chkconfig: 2345 99 10
-### END INIT INFO
-
-# Source LSB function library.
-
 [Unit]
 Description= jwala service file
 After=network.target
 
 [Service]
 Type=forking
+PIDFile="@TOMCAT_HOME@/logs/catalina.pid"
 
 # Define the tomcat username
 User=tomcat
-
-#Environment="JAVA_HOME=/usr/lib/jvm/jre"
-#Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+Group=tomcat
 
 Environment="CATALINA_BASE=@TOMCAT_HOME@"
 Environment="CATALINA_HOME=@TOMCAT_HOME@"
 Environment="CATALINA_PID=@TOMCAT_HOME@/logs/catalina.pid"
 Environment="CATALINA_HOME_BIN=@TOMCAT_HOME@/bin"
 
-ExecStart=/opt/ctp/jwala-0.0.245/apache-tomcat-7.0.55/bin/startup.sh
-ExecStop=/opt/ctp/jwala-0.0.245/apache-tomcat-7.0.55/bin/catalina.sh 
+ExecStart="@TOMCAT_HOME@/bin/startup.sh"
+ExecStop=@TOMCAT_HOME@/bin/catalina.sh stop
 
 [Install]
 WantedBy=multi-user.target
