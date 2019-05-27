ff892c46256adc280587cd702bb7587a8d2e053d
Kselvara <karthickprakash1991@gmail.com>
2019-05-27 14:31:34 +0530

    Removed Backspace function as it used twice

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/ResourceErrorHandlingRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/configuration/ResourceErrorHandlingRunSteps.java
@@ -30,7 +30,6 @@ public class ResourceErrorHandlingRunSteps {
         final String normalizedText = text.charAt(0) == '"' ? text.substring(1, text.length() - 1) : text;
         By by = By.xpath("(//pre[contains(@class, 'CodeMirror-line')]//span[text()='" + normalizedText + "'])[1]");
         jwalaUi.click(by);
-        jwalaUi.backSpace();
         jwalaUi.sendKeysViaActions(Keys.BACK_SPACE);
     }
 
